24b2fc87dfb632ac6394461a70875f452f5eaeca
todddrinkwater <todddrinkwaternz@gmail.com>
2017-07-14 09:30:14 +1200

    server working

--- routes.js
+++ routes.js
@@ -1,10 +1,14 @@
 var express = require('express')
 var router = express.Router()
-
+var data = require("./data.json")
 
 
 router.get("/", function(req, res){
   res.send("hello tacossssss")
+})
+
+router.get("/index", function (req, res) {
+  res.render("tacos/index", data)
 })
 
 module.exports = router
