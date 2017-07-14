a7cab156502f1d4d3b477a5a3e4747fe9bc7a105
todddrinkwater <todddrinkwaternz@gmail.com>
2017-07-14 12:28:58 +1200

    Routes now working

--- routes.js
+++ routes.js
@@ -1,7 +1,8 @@
 var express = require('express')
 var router = express.Router()
 var data = require("./data.json")
-
+var order = require("./order.json")
+var fs = require('fs')
 
 router.get("/", function(req, res){
 
@@ -13,6 +14,10 @@ router.get("/index", function (req, res) {
   res.render("tacos/index", data)
 })
 
+router.get("/index/view/yourOrder", function (req, res) {
+  res.render("tacos/order", order)
+})
+
 router.get('/index/view/:id', function(req, res) {
   var id = req.params.id
   var orderTaco = data.tacos.find(function(taco) {
@@ -21,5 +26,17 @@ router.get('/index/view/:id', function(req, res) {
   res.render('tacos/view', orderTaco)
 })
 
+
+router.post('/index/view/:id', function(req, res) {
+  var id = req.params.id
+  var editOrder = req.body
+
+console.log(editOrder);
+
+  fs.writeFile("order.json", JSON.stringify(editOrder), function (err) {
+    if (err) throw err
+    res.redirect("/index/view/yourOrder")
+  })
+})
 
 module.exports = router
