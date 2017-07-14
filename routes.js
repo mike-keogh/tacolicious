d6a15cc7db959b437cb5dbd8cf2c9c6654c40f8c
todddrinkwater <todddrinkwaternz@gmail.com>
2017-07-14 15:54:06 +1200

    list added on order

--- routes.js
+++ routes.js
@@ -1,29 +1,45 @@
 var express = require('express')
 var router = express.Router()
-var data = require("./data.json")
-var order = require("./order.json")
 var fs = require('fs')
 
+function readOrder(callback) {
+  fs.readFile('./order.json', 'utf8', (err, order) => {
+    if (err) throw err
+    callback(JSON.parse(order))
+  })
+}
+
+function readData(callback) {
+  fs.readFile('./data.json', 'utf8', (err, data) => {
+    if (err) throw err
+    callback(JSON.parse(data))
+  })
+}
+
 router.get("/", function(req, res){
-
   res.redirect('/index')
-
 })
 
 router.get("/index", function (req, res) {
-  res.render("tacos/index", data)
+  readData(function(data) {
+    res.render("tacos/index", data)
+  })
 })
 
 router.get("/index/view/yourOrder", function (req, res) {
-  res.render("tacos/order", order)
+  readOrder(function(order) {
+    res.render("tacos/order", order)
+  })
 })
 
 router.get('/index/view/:id', function(req, res) {
   var id = req.params.id
-  var orderTaco = data.tacos.find(function(taco) {
-    return taco.id == id
+  readData(function(data) {
+    var orderTaco = data.tacos.find(function(taco) {
+      return taco.id == id
+    })
+    res.render('tacos/view', orderTaco)
   })
-  res.render('tacos/view', orderTaco)
 })
 
 
